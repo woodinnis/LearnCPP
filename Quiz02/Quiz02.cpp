b71cfd244a86a0d5e6de2e07692c7349b9b55154
wood_innis <wood.innis@gmail.com>
2016-07-29 18:40:22 -0700

    Section 02 Quizzes 01 - Calculator using doubles 02 - Time-to-ground using constants (incomplete)

---
+++ Quiz02/Quiz02.cpp
@@ -0,0 +1,16 @@
+// Quiz02.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+const double GRAVITY(9.8);
+
+int main()
+{
+	
+    return 0;
+}
+
