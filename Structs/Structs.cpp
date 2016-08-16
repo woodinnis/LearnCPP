c3087025a99ff1e94e293ed1ef80835d2249cfa2
wood_innis <wood.innis@gmail.com>
2016-08-16 11:47:12 -0700

    Typedefs completed. Structs beginning, core console added.

---
+++ Structs/Structs.cpp
@@ -0,0 +1,13 @@
+// Structs.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
--- Typedefs/Typedefs.cpp
+++ Typedefs/Typedefs.cpp
@@ -6,9 +6,26 @@
 
 using namespace std;
 
+// Two ways to define a typedef
+typedef double porcupine_t;
+using pi_t = double;
+
+using error_t = int;
+
+error_t printData() {
+
+}
 
 int main()
 {
+	int8_t i(97);	// int_8 is a predefined typedef for char
+	porcupine_t porky(30.1);
+	pi_t pi(3.14159);
+
+	cout << i << endl;
+	cout << porky << endl;
+	cout << pi << endl;
+
     return 0;
 }
 
