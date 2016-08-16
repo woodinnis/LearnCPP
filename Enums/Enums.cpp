f67f68e9523c7cd81206daa6a2fa9431a4fbe380
wood_innis <wood.innis@gmail.com>
2016-08-15 20:42:04 -0700

    The enumerated types section Along with the quiz.

--- Enums/Enums.cpp
+++ Enums/Enums.cpp
@@ -6,8 +6,18 @@
 
 using namespace std;
 
+enum Monster {
+	MONSTER_ORC,
+	MONSTER_GOBLIN,
+	MONSTER_TROLL,
+	MONSTER_OGRE,
+	MONSTER_SKELETON
+};
+
 int main()
 {
+	Monster monsterOne = MONSTER_TROLL;
+
     return 0;
 }
 
