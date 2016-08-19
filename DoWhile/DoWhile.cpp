6497b03bd1c00dce23c1ae7387489b2cea33a1f6
woodinnis <wood.innis@gmail.com>
2016-08-18 20:28:26 -0700

    DoWhile loop example program with comments

--- DoWhile/DoWhile.cpp
+++ DoWhile/DoWhile.cpp
@@ -9,6 +9,22 @@ using namespace std;
 int main()
 {
 
-    return 0;
+	// Declare the condition variable outside the loop
+	int selection;
+
+	// The block executes at least once before ever checking the condition
+	do {
+		cout << "Please make a selection: \n";
+		cout << "1) Addition\n";
+		cout << "2) Subtraction\n";
+		cout << "3) Multiplication\n";
+		cout << "4) Division\n";
+		cin >> selection;
+	}
+	// The condition is checked here
+	while (selection != 1 && selection != 2 && selection != 3 && selection != 4);
+    
+	cout << "You selected option #" << selection << "\n";
+	return 0;
 }
 
