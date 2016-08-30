9ca61d7522d491b6aa54020d73d084862f42d983
wood_innis <wood.innis@gmail.com>
2016-08-30 12:55:23 -0700

    Quiz for Section 7.4

---
+++ 7_4ArgumentAndReturnQuiz/7_4ArgumentAndReturnQuiz.cpp
@@ -0,0 +1,37 @@
+// 7_4ArgumentAndReturnQuiz.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <array>
+#include <algorithm>
+#include <vector>
+
+struct Employee {};
+
+int sumTo(int maxInt) {
+	// 1) A function named sumTo() that takes an integer parameter and returns the sum of all the numbers between 1 and the input number.
+}
+
+void printEmployeeName(const Employee &employee) {
+	// 2) A function named printEmployeeName() that takes an Employee struct as input.
+}
+
+void minMax(const int one, const int two, int &minOut, int &maxOut) {
+	// 3) A function named minmax() that takes two integers as input and returns the smaller and larger number as separate parameters.
+}
+
+int getIndexOfLargestValue(const int *array, int size) {
+	// 4) A function named getIndexOfLargestValue() that takes an integer array (as a pointer) and an array size, and returns the index of the largest element in the array.
+}
+
+int& getElement(const int *array, int index) {
+	// 5) A function named getElement() that takes an integer array (as a pointer) and an index and returns the array element at that index (not a copy). Assume the index is valid, and the return value is const.
+}
+int main()
+{
+
+    return 0;
+}
+
