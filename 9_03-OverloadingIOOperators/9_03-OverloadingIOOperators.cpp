61be76a5c5b564784317d308a99bc03d66ed430a
wood_innis <wood.innis@gmail.com>
2016-09-12 13:10:20 -0700

    Section 9.3 Quiz complete

--- 9_03-OverloadingIOOperators/9_03-OverloadingIOOperators.cpp
+++ 9_03-OverloadingIOOperators/9_03-OverloadingIOOperators.cpp
@@ -3,9 +3,90 @@
 
 #include "stdafx.h"
 
+using namespace std;
+//	Take the Fraction class we wrote in the previous quiz (listed below) and add an overloaded operator<< and operator>> to it.
 
-int main()
+class Fraction
 {
-    return 0;
+private:
+	int m_numerator = 0;
+	int m_denominator = 1;
+
+public:
+	Fraction(int numerator = 0, int denominator = 1) :
+		m_numerator(numerator), m_denominator(denominator)
+	{
+		// We put reduce() in the constructor to ensure any fractions we make get reduced!
+		// Since all of the overloaded operators create new Fractions, we can guarantee this will get called here
+		reduce();
+	}
+
+	static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a % b);
+	}
+
+	void reduce()
+	{
+		int gcd = Fraction::gcd(m_numerator, m_denominator);
+		m_numerator /= gcd;
+		m_denominator /= gcd;
+	}
+
+	friend Fraction operator*(const Fraction &f1, const Fraction &f2);
+	friend Fraction operator*(const Fraction &f1, int value);
+	friend Fraction operator*(int value, const Fraction &f1);
+	friend ostream& operator<<(ostream &out, const Fraction &fraction);
+	friend istream& operator>>(istream &in, Fraction &fraction);
+
+	void print()
+	{
+		std::cout << m_numerator << "/" << m_denominator << "\n";
+	}
+};
+
+Fraction operator*(const Fraction &f1, const Fraction &f2)
+{
+	return Fraction(f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator);
 }
 
+Fraction operator*(const Fraction &f1, int value)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
+}
+
+Fraction operator*(int value, const Fraction &f1)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
+}
+
+ostream & operator<<(ostream & out, const Fraction & fraction)
+{
+	out << fraction.m_numerator << "/" << fraction.m_denominator;
+	return out;
+}
+
+istream & operator >> (istream & in, Fraction & fraction)
+{
+	char c;
+
+	in >> fraction.m_numerator;
+	in >> c;
+	in >> fraction.m_denominator;
+	return in;
+}
+int main()
+{
+
+	Fraction f1;
+	std::cout << "Enter fraction 1: ";
+	std::cin >> f1;
+
+	Fraction f2;
+	std::cout << "Enter fraction 2: ";
+	std::cin >> f2;
+
+	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n';
+
+	return 0;
+}
