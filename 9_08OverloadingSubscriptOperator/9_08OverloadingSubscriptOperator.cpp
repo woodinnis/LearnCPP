b856c99004d537d098dbfa73429f04a52a35a225
wood_innis <wood.innis@gmail.com>
2016-09-13 11:19:43 -0700

    Section 9.8 Quiz started

---
+++ 9_08OverloadingSubscriptOperator/9_08OverloadingSubscriptOperator.cpp
@@ -0,0 +1,14 @@
+// 9_08OverloadingSubscriptOperator.cpp : Defines the entry point for the console application.
+//
+
+//	1) A map is a class that stores elements as a key-value pair. The key must be unique, and is used to access the associated pair. 
+//	In this quiz, we’re going to write an application that lets us assign grades to students by name, using a simple map class. 
+//	The student’s name will be the key, and the grade (as a char) will be the value.
+
+#include "stdafx.h"
+
+int main()
+{
+    return 0;
+}
+
---
+++ 9_08OverloadingSubscriptOperator/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// 9_08OverloadingSubscriptOperator.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
