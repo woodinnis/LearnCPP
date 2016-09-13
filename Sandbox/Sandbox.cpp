15ae64fe633c81c874eefe0737d7ea19d324e03e
wood_innis <wood.innis@gmail.com>
2016-09-13 11:14:55 -0700

    Sandbox changes

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -7,8 +7,98 @@
 #include "Potato.h"
 #include "TestHeader.h"
 #include "Cents.h"
+#include <cassert> // for assert()
+
+class IntList
+{
+private:
+	int m_list[10];
+
+public:
+	int& operator[] (const int index);
+};
+
+int& IntList::operator[] (const int index)
+{
+	assert(index >= 0 && index < 10);
+
+	return m_list[index];
+}
 
 using namespace std;
+
+class Digit
+{
+private:
+	int m_digit;
+public:
+	Digit(int digit = 0)
+		: m_digit(digit)
+	{
+	}
+
+	Digit& operator++(); // prefix
+	Digit& operator--(); // prefix
+
+	Digit operator++(int); // postfix
+	Digit operator--(int); // postfix
+
+	friend std::ostream& operator<< (std::ostream &out, const Digit &d);
+};
+
+Digit& Digit::operator++()
+{
+	// If our number is already at 9, wrap around to 0
+	if (m_digit == 9)
+		m_digit = 0;
+	// otherwise just increment to next number
+	else
+		++m_digit;
+
+	return *this;
+}
+
+Digit& Digit::operator--()
+{
+	// If our number is already at 0, wrap around to 9
+	if (m_digit == 0)
+		m_digit = 9;
+	// otherwise just decrement to next number
+	else
+		--m_digit;
+
+	return *this;
+}
+
+Digit Digit::operator++(int)
+{
+	// Create a temporary variable with our current digit
+	Digit temp(m_digit);
+
+	// Use prefix operator to increment this digit
+	++(*this); // apply operator
+
+			   // return temporary result
+	return temp; // return saved state
+}
+
+Digit Digit::operator--(int)
+{
+	// Create a temporary variable with our current digit
+	Digit temp(m_digit);
+
+	// Use prefix operator to decrement this digit
+	--(*this); // apply operator
+
+			   // return temporary result
+	return temp; // return saved state
+}
+
+std::ostream& operator<< (std::ostream &out, const Digit &d)
+{
+	out << d.m_digit;
+	return out;
+}
 
 int main()
 {
@@ -32,6 +122,21 @@ int main()
 		cout << "a dime is greater than a nickle.\n";
 	if (nickle <= dime)
 		cout << "a dime is greater than or equal to a nickle.\n";
+
+	Digit digit(c2);
+
+	std::cout << digit;
+	std::cout << ++digit; // calls Digit::operator++();
+	std::cout << digit++; // calls Digit::operator++(int);
+	std::cout << digit;
+	std::cout << --digit; // calls Digit::operator--();
+	std::cout << digit--; // calls Digit::operator--(int);
+	std::cout << digit;
+
+	IntList list;
+
+	return 0;
+
 
 	/*
 	bool createPotato = true;
