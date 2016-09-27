28cd23cbd31f6ee782369e8ff15368134fe36145
wood_innis <wood.innis@gmail.com>
2016-09-27 12:46:55 -0700

    Section 10.2 Completed main()

--- 10_02-Composition/10_02-Composition.cpp
+++ 10_02-Composition/10_02-Composition.cpp
@@ -2,10 +2,37 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
+#include <iostream>
+#include "creature.h"
+#include "point2d.h"
 
 int main()
 {
-    return 0;
-}
+	using namespace std;
+	cout << "Enter a name for your creature: ";
+	std::string name;
+	cin >> name;
+	Creature creature(name, Point2D(4, 7));
 
+	while (1) {
+		// Print the creature's name and position
+		cout << creature << endl;
+		
+		cout << "Enter new X coordinate (-1 to quit): ";
+		int x = 0;
+		cin >> x;
+		if (x == -1)
+			break;
+
+		cout << "Enter new Y coordinate (-1 to quit): ";
+		int y = 0;
+		cin >> y;
+		if (y == -1)
+			break;
+
+		creature.moveTo(x, y);
+	}
+
+	return 0;
+}
